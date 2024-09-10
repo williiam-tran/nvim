dc1ee960a919e3ce601a4025f72472a69b516a89
Tung Tran <tt.tung261@gmail.com>
2024-09-10 23:37:23 +0700

    Update neogit

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -1,4 +1,27 @@
 return {
+	{
+		"amitds1997/remote-nvim.nvim",
+		version = "*", -- Pin to GitHub releases
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- For standard functions
+			"MunifTanjim/nui.nvim", -- To build the plugin UI
+			"nvim-telescope/telescope.nvim", -- For picking b/w different remote methods
+		},
+		config = true,
+	},
+	{
+		"NeogitOrg/neogit",
+		lazy = false,
+		dependencies = {
+			"nvim-lua/plenary.nvim", -- required
+			"sindrets/diffview.nvim", -- optional - Diff integration
+
+			-- Only one of these is needed, not both.
+			"nvim-telescope/telescope.nvim", -- optional
+			"ibhagwan/fzf-lua", -- optional
+		},
+		config = true,
+	},
 	"lewis6991/gitsigns.nvim",
 	"chrisgrieser/nvim-various-textobjs",
 	{
